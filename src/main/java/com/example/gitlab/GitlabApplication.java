fce381f76ee6ed58edf50a171d2ee4da0de1fda6
Nam <admin@Nam>
2023-01-27 09:59:47 +0700

    update

---
+++ src/main/java/com/example/gitlab/GitlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabApplication.class, args);
+	}
+
+}
